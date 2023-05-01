7bbb486719d1f38648a747234f12df1d1a6f1f79
g-ionov <g.ionoff@yandex.ru>
2023-05-01 17:00:47 +0300

    Added recalculation of price in Celery task

--- service/services/models.py
+++ service/services/models.py
@@ -1,9 +1,21 @@
 from django.db import models
 from django.core.validators import MaxValueValidator
 
+from services.tasks import set_price
+
 class Service(models.Model):
     name = models.CharField(max_length=50)
     full_price = models.PositiveIntegerField()
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.__original_price = self.full_price
+
+    def save(self, *args, **kwargs):
+        if self.__original_price != self.full_price:
+            for subscription in self.subscriptions.all():
+                set_price.delay(subscription.id)
+        super().save(*args, **kwargs)
 
 
 class Plan(models.Model):
@@ -16,8 +28,19 @@ class Plan(models.Model):
     plan_type = models.CharField(max_length=10, choices=PLAN_TYPES)
     discount_percent = models.PositiveIntegerField(default=0, validators=[MaxValueValidator(100)])
 
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.__original_discount_percent = self.discount_percent
+
+    def save(self, *args, **kwargs):
+        if self.__original_discount_percent != self.discount_percent:
+            for subscription in self.subscriptions.all():
+                set_price.delay(subscription.id)
+        super().save(*args, **kwargs)
+
 
 class Subscription(models.Model):
     client = models.ForeignKey('clients.Client', on_delete=models.PROTECT, related_name='subscriptions')
     service = models.ForeignKey(Service, on_delete=models.PROTECT, related_name='subscriptions')
     plan = models.ForeignKey(Plan, on_delete=models.PROTECT, related_name='subscriptions')
+    price = models.PositiveIntegerField(default=0)
